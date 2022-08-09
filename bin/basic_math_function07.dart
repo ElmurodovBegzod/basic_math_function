75754d0b50ab681baab0e760e291aaf4a32889fc
Begzod <ebegzod477@gmail.com>
2022-08-09 20:25:11 +0500

    code07

---
+++ bin/basic_math_function07.dart
@@ -0,0 +1,19 @@
+/*
+Create function named func with a argument
+  given the argument number return its how many 
+  Args:
+      x (int): the radian number
+  Returns:
+      result (int): return lenght
+*/
+int func(int a) {
+  String s;
+  int n;
+  s = a.toString();
+  n = s.length;
+  return n;
+}
+
+void main() {
+  print(func(7777777));
+}
